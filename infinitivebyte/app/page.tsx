185dad2897fd39a137f4ca1e9b62f0a6eb111852
Rachid Mouhcine <rmouhcin@e2r5p15.1337.ma>
2025-11-25 19:09:26 +0100

    II

--- infinitivebyte/app/page.tsx
+++ infinitivebyte/app/page.tsx
@@ -7,10 +7,12 @@ import { Agency } from "./api/data/route";
 export default function Home() {
 
 	const [Agencies, setAgencies] = useState<Agency[] | null>(null);
+	const [Contacts, setContacts] = useState<any>(null);
 
 	useEffect(()=>{
 		async function Get(){
 			const res = await axios.get("/api/data?target=agencies");
+			const res = await axios.get("/api/data?target=contacts");
 			console.log(res.data);
 			setAgencies(res.data?.Agencies);
 		}
